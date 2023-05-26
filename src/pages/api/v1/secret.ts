17adfdef1be7cf4cbbdb08eb2472d3b3cddff84b
FaisalAl-Tameemi <faisal.al.tameemi@gmail.com>
2023-05-27 03:03:31 +0400

    Check for Authorization header alongside the session cookie

--- src/pages/api/v1/secret.ts
+++ src/pages/api/v1/secret.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "./[...nextauth]"
+import { getToken } from 'next-auth/jwt'
 
 type Data = {
   secret: string
@@ -15,9 +16,17 @@ const handler = async (
     return res.status(405).end()
   }
 
+  // Check either the session or Bearer token.
+  // 
+  // This approach allows experimentation with the API, however, in a production scenario
+  // this should ideally be either a session or a token verification, depending on the use case.
+  // 
+  // This logic would normally be placed in a middleware method such that it can be reused 
+  // for multiple protected API routes.
   const session = await getServerSession(req, res, authOptions)
+  const token = await getToken({ req, secret: process.env.JWT_SECRET! })
 
-  if (!session) {
+  if (!session && !token) {
     return res.status(401).end()
   }
 
