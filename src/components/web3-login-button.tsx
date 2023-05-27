5aa646d8b7e36e044d09510c158a4e3f9eb61975
FaisalAl-Tameemi <faisal.al.tameemi@gmail.com>
2023-05-28 02:16:32 +0400

    Add AntDesign; Re-organize some components and styles

--- src/components/web3-login-button.tsx
+++ src/components/web3-login-button.tsx
@@ -3,54 +3,58 @@ import { signIn } from 'next-auth/react'
 import { WalletSelect } from '@talismn/connect-components'
 import { Wallet, WalletAccount } from '@talismn/connect-wallets'
 import { getMessageToSign } from '@/utils/signature'
+import { Button } from 'antd'
 
 interface Web3LoginButtonProps {
-    dappName: string,
-    message?: string,
+  dappName: string,
+  message?: string,
+  button?: React.ReactElement,
 }
 
 export default function Web3LoginButton(props: Web3LoginButtonProps) {
-    const [wallet, setWallet] = useState<Wallet | null>(null)
-    const [accounts, setAccounts] = useState<WalletAccount[]>([])
+  const [wallet, setWallet] = useState<Wallet | null>(null)
+  const [accounts, setAccounts] = useState<WalletAccount[]>([])
 
-    // Once both the wallet and the selected account is known
-    // we prompt the user to sign a message which will be sent to the API
-    // for verification and to obtain a JWT token
-    useEffect(() => {
-        if (wallet && accounts.length > 0) {
-            const accountAddress = accounts[0].address
-            const message = props.message || getMessageToSign(accountAddress)
+  // Once both the wallet and the selected account is known
+  // we prompt the user to sign a message which will be sent to the API
+  // for verification and to obtain a JWT token
+  useEffect(() => {
+    if (wallet && accounts.length > 0) {
+      const accountAddress = accounts[0].address
+      const message = props.message || getMessageToSign(accountAddress)
 
-            wallet.signer.signRaw({
-                type: 'payload',
-                data: message,
-                address: accountAddress,
-            }).then(async ({ signature }: { signature: string }) => {
-                // validate signature via API to obtain session
-                await signIn('credentials', {
-                    address: accountAddress,
-                    message,
-                    signature,
-                    redirect: true,
-                    callbackUrl: '/secrets'
-                })
-            })
-        }
-    }, [wallet, accounts, props.message])
+      wallet.signer.signRaw({
+        type: 'payload',
+        data: message,
+        address: accountAddress,
+      }).then(async ({ signature }: { signature: string }) => {
+        // validate signature via API to obtain session
+        await signIn('credentials', {
+          address: accountAddress,
+          message,
+          signature,
+          redirect: true,
+          callbackUrl: '/secrets'
+        })
+      })
+    }
+  }, [wallet, accounts, props.message])
 
-    return (
-        <WalletSelect
-            dappName={props.dappName}
-            triggerComponent={
-                <button style={{ padding: 12 }}>
-                    Signin with Wallet
-                </button>
-            }
-            onWalletConnectClose={() => console.log('onWalletConnectClose')}
-            onWalletSelected={(wallet) => setWallet(wallet)}
-            onUpdatedAccounts={(accounts) => setAccounts(accounts || [])}
-            onAccountSelected={(account) => console.log('onAccountSelected', account)}
-            onError={(error) => console.log('onError', error)}
-        />
-    )
+  const DefaultButton = (
+    <Button type='primary'>
+      Signin with Wallet
+    </Button>
+  )
+
+  return (
+    <WalletSelect
+      dappName={props.dappName}
+      triggerComponent={props.button || DefaultButton}
+      onWalletConnectClose={() => console.log('onWalletConnectClose')}
+      onWalletSelected={(wallet) => setWallet(wallet)}
+      onUpdatedAccounts={(accounts) => setAccounts(accounts || [])}
+      onAccountSelected={(account) => console.log('onAccountSelected', account)}
+      onError={(error) => console.log('onError', error)}
+    />
+  )
 }
